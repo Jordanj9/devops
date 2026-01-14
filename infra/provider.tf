9eaebc10c997bfd116383efab8bbd38fda0eb7ef
Jordanj9 <jordan_j9@hotmail.com>
2026-01-14 06:56:26 -0500

    infraestructura como codigo

---
+++ infra/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+  subscription_id = "952e97aa-77ae-45e2-8c1e-b9ee0cfbdc6d"
+}
